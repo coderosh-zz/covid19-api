6716783cf09bf9b9cdb89f4bf50f4d56f4b9d7aa
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-03-27 21:48:09 +0545

    cors

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ app.use(express.json())
 
 const byCountriesRouter = require('./routes/bycountries')
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use('/', byCountriesRouter)
 
 const PORT = process.env.PORT || 4000
